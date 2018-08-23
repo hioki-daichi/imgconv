9dfd2486997fd9527ff00e5b675ef07713db8940
Daichi Hioki <hiokidaichi@gmail.com>
2018-08-23 23:57:37 +0900

    Add test to cmd.Run

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,1 +1,77 @@
 package cmd
+
+import (
+	"bytes"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hioki-daichi/imgconv/conversion"
+	"github.com/hioki-daichi/imgconv/fileutil"
+)
+
+func TestCmd_Run(t *testing.T) {
+	jpegDecoder := &conversion.Jpeg{}
+	pngDecoder := &conversion.Png{}
+	gifDecoder := &conversion.Gif{}
+
+	jpegEncoder := &conversion.Jpeg{Options: &jpeg.Options{Quality: 1}}
+	pngEncoder := &conversion.Png{Encoder: &png.Encoder{CompressionLevel: png.NoCompression}}
+	gifEncoder := &conversion.Gif{Options: &gif.Options{NumColors: 1}}
+
+	cases := []struct {
+		decoder  conversion.Decoder
+		encoder  conversion.Encoder
+		force    bool
+		expected func(string) string
+	}{
+		{decoder: jpegDecoder, encoder: pngEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/jpeg/sample1.png"\nConverted: "` + tempdir + `/jpeg/sample2.png"\nConverted: "` + tempdir + `/jpeg/sample3.png"\n`
+		}},
+		{decoder: jpegDecoder, encoder: gifEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/jpeg/sample1.gif"\nConverted: "` + tempdir + `/jpeg/sample2.gif"\nConverted: "` + tempdir + `/jpeg/sample3.gif"\n`
+		}},
+		{decoder: pngDecoder, encoder: jpegEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/png/sample1.jpg"\nConverted: "` + tempdir + `/png/sample2.jpg"\n`
+		}},
+		{decoder: pngDecoder, encoder: gifEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/png/sample1.gif"\nConverted: "` + tempdir + `/png/sample2.gif"\n`
+		}},
+		{decoder: gifDecoder, encoder: jpegEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/gif/sample1.jpg"\n`
+		}},
+		{decoder: gifDecoder, encoder: pngEncoder, force: true, expected: func(tempdir string) string {
+			return `Converted: "` + tempdir + `/gif/sample1.png"\n`
+		}},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			runner := Runner{OutStream: buf, Decoder: c.decoder, Encoder: c.encoder, Force: c.force}
+			withTempDir(t, func(t *testing.T, tempdir string) {
+				expectedStr := c.expected(tempdir)
+
+				err := runner.Run(tempdir)
+				if err != nil {
+					t.FailNow()
+				}
+
+				if bufStr := buf.String(); !regexp.MustCompile(expectedStr).MatchString(bufStr) {
+					t.Errorf("expected: %s, actual: %s", expectedStr, bufStr)
+				}
+			})
+		})
+	}
+}
+
+func withTempDir(t *testing.T, f func(t *testing.T, tempdir string)) {
+	tempdir, _ := ioutil.TempDir("", "imgconv")
+	fileutil.CopyDirRec("../testdata/", tempdir)
+	defer os.RemoveAll(tempdir)
+	f(t, tempdir)
+}
